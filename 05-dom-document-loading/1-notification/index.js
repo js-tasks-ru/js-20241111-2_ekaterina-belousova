d6324452c235e4a1720b2e1252a7e6a2139ec2dc
Ekaterina Belousova <lapilapka@yahoo.com>
2025-01-11 19:44:49 +0300

    Add task 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,48 @@
 export default class NotificationMessage {
+  static lastNotificationShown;
+  element;
+  timer;
 
+  constructor(message, {duration, type} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createNotificationTemplate(this.createNotification());
+  }
+
+  show() {
+    if (NotificationMessage.lastNotificationShown) {
+      NotificationMessage.lastNotificationShown.destroy();
+    }
+    document.body.appendChild(this.element);
+    NotificationMessage.lastNotificationShown = this;
+    this.timer = setTimeout(() => this.remove(), this.duration);
+  }
+
+  createNotificationTemplate(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createNotification() {
+    return `  <div class="notification ${this.type}" style="--value:${this.duration}ms">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+  </div>`;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    clearTimeout(this.timer);
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,116 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  element;
+  subElements = {};
 
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.renderTable();
+    this.selectSubElements();
+  }
+
+  renderTable() {
+    this.element = this.addTemplate(this.createTable());
+  }
+
+  selectSubElements() {
+    this.element
+      .querySelectorAll("[data-element]")
+      .forEach(
+        (element) => (this.subElements[element.dataset.element] = element)
+      );
+  }
+
+  addTemplate(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createTableHeader() {
+    return `<div data-element="header" class="sortable-table__header sortable-table__row">
+    ${this.headerConfig.map(elem => `<div class="sortable-table__cell" data-id="${elem.id}" data-sortable="${elem.sortable}" data-order="asc">
+    <span>${elem.title}</span>
+    <span data-element="arrow" class="sortable-table__sort-arrow">
+    <span class="sort-arrow"></span>
+    </span></div>`).join('')}</div>`;
+  }
+
+  createTableRow(elem) {
+    return `
+    <a href="/products/${elem.id}" class="sortable-table__row">
+    ${this.headerConfig
+      .map((column) => this.createTableCell(elem, column))
+      .join("")}
+  </a>`;
+  }
+
+  createTableCell(elem, column) {
+    if (column.template) {
+      return column.template(elem.images);
+    } else {
+      return `<div class="sortable-table__cell">${elem[column.id]}</div>`;
+    }
+  }
+
+  createTableBody() {
+    return `
+    <div data-element="body" class="sortable-table__body">
+        ${this.data.map((elem) => this.createTableRow(elem)).join("")}
+    </div>
+          `;
+  }
+
+  sort(field, order) {
+    const sortedData = this.sortData(field, order);
+
+    this.subElements.body.innerHTML = this.createTableBody(sortedData);
+
+  }
+
+  sortData(field, order) {
+    const arr = [...this.data];
+    const column = this.headerConfig.find(item => item.id === field);
+    const {sortType} = column;
+    const directions = {
+      asc: 1,
+      desc: -1
+    };
+
+    const direction = directions[order];
+
+
+    return arr.sort((a, b) => {
+      switch (sortType) {
+      case 'number':
+        return direction * (a[field] - b[field]);
+      case 'string':
+        return direction * a[field].localeCompare(b[field], ['ru', 'en']);
+      }
+    });
+  }
+
+  createTable() {
+    return `
+    <div class="sortable-table">
+    ${this.createTableHeader()}
+    ${this.createTableBody()}
+    </div>
+    <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+    <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+      <div>
+        <p>No products satisfies your filter criteria</p>
+        <button type="button" class="button-primary-outline">Reset all filters</button>
+      </div>
+    </div>`;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
 
